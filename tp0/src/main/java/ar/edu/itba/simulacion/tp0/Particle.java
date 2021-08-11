0b949978ff867103f7b5da2f8c113444f5c21d10
Tobias Brandy <tbrandy@itba.edu.ar>
2021-08-11 17:13:50 -0300

    Particle: collides y equals

--- tp0/src/main/java/ar/edu/itba/simulacion/tp0/Particle.java
+++ tp0/src/main/java/ar/edu/itba/simulacion/tp0/Particle.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.simulacion.tp0;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Particle {
@@ -18,17 +20,51 @@ public class Particle {
         // Serialization
     }
 
-    public Particle(int id, double x, double y, double radius) {
+    public Particle(final int id, final double x, final double y, final double radius) {
         this.id = id;
         this.x = x;
         this.y = y;
         this.radius = radius;
     }
 
-    public double distanceTo(Particle other) {
+    public double distanceTo(final Particle other) {
         double deltaX = this.x - other.x;
         double deltaY = this.y - other.y;
         return Math.sqrt(deltaX*deltaX + deltaY*deltaY) - this.radius - other.radius;
+    }
+
+    public boolean collides(final Particle particle) {
+        return distanceTo(particle) <= 0;
+    }
+
+    public boolean collides(final Collection<Particle> particles) {
+        return particles.stream().anyMatch(this::collides);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Particle)) return false;
+        final Particle particle = (Particle) o;
+        return id == particle.id &&
+            Double.compare(particle.x, x) == 0 &&
+            Double.compare(particle.y, y) == 0 &&
+            Double.compare(particle.radius, radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, x, y, radius);
+    }
+
+    @Override
+    public String toString() {
+        return "Particle{" +
+            "id=" + id +
+            ", x=" + x +
+            ", y=" + y +
+            ", radius=" + radius +
+            '}';
     }
 
     //////////////////////////// Burocracia //////////////////////////////////////
