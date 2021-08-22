7ebcfe8460f81b5980a21c3b4fd939b08f4d5a76
tobias <tbrandy@itba.edu.ar>
2021-08-22 11:37:13 -0300

    TP2: imports faltantes, aprovechamos lombock para config y retornamos una simulacion de 100 steps para poder ir avanzando con los graficos
    Cambios de config:
     - M ya no hace falta, usamos el optimo
     - Elimino time, pues no me parece que tenga que ser un time fijo la simulacion, sino cortar por alguna condicion heuristica nuestra

--- tp2/tp2_java/src/main/java/ar/edu/itba/simulacion/tp2/TP2.java
+++ tp2/tp2_java/src/main/java/ar/edu/itba/simulacion/tp2/TP2.java
@@ -2,51 +2,53 @@ package ar.edu.itba.simulacion.tp2;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import ar.edu.itba.simulacion.particle.Particle2D;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
-public class TP2 {
-    
-    public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
-        
+public final class TP2 {
+
+    // Temporal, para probar graficar
+    private static final int STEPS = 100;
+
+    private TP2() {
+        // static class
+    }
+
+    public static void main(String[] args) throws IOException {
         if(args.length < 1) {
             throw new IllegalArgumentException("First argument must be config path");
         }
         final ObjectMapper mapper = new ObjectMapper();
         
-        final OffLaticeConfig config = mapper.readValue(new File(args[0]), OffLaticeConfig.class);
+        final OffLatticeConfig config = mapper.readValue(new File(args[0]), OffLatticeConfig.class);
 
-        List<Particle2D> particles = Arrays
-            .asList(mapper.readValue(new File(config.particlesFile), Particle2D[].class));
-
-           
-      
-
-    }
-
-    public static class OffLaticeConfig {
-        public int          time;
-        public int          M;
-        public double       L;
-        public double       actionRadius;
-        public String       particlesFile;
-        public String       outputFile;
-
-        private OffLaticeConfig() {
-            //Deserialization
+        final List<Particle2D> particles = List.of(mapper.readValue(new File(config.particlesFile), Particle2D[].class));
+        if(particles.isEmpty()) {
+            throw new IllegalArgumentException("No particles found");
         }
 
-        public OffLaticeConfig(int time, int M, double L, double actionRadius, String particlesFile, String outputFile) {
-            this.time = time;
-            this.M = M;
-            this.L = L;
-            this.actionRadius = actionRadius;
-            this.particlesFile = particlesFile;
-            this.outputFile = outputFile;
-        }      
+        final double maxRadius = particles.stream().mapToDouble(Particle2D::getRadius).max().orElseThrow();
+
+        final OffLattice automata = new OffLattice(config.spaceWidth, config.actionRadius, config.periodicBorder, maxRadius);
+
+        final List<List<Particle2D>> automataStates = automata.doNSteps(particles, STEPS);
+
+        mapper.writeValue(new File(config.outputFile), automataStates);
+    }
+
+    @Data
+    @Jacksonized
+    @Builder(setterPrefix = "with")
+    public static class OffLatticeConfig {
+        public double       spaceWidth;
+        public double       actionRadius;
+        public boolean      periodicBorder;
+        public String       particlesFile;
+        public String       outputFile;
     }
 }
