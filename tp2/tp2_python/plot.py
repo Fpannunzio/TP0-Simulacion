9cc2cc51929e5ceac9c3ec1fef57765794724f25
Ignacio Sagues <isagues@itba.edu.ar>
2021-08-26 01:27:46 -0300

    Correccion del plot
    Los angulos vienen negativos entonces hay que sumar PI antes de dividir para que se use toda la paleta de color
    
    Agrego scatter para que se vea el punto de donde sale el vector.

--- tp2/tp2_python/plot.py
+++ tp2/tp2_python/plot.py
@@ -5,6 +5,8 @@ import matplotlib.cm as cm
 import matplotlib.animation as animation
 
 import numpy as np
+import math
+from numpy.core.fromnumeric import size
 
 from models import Particle
 
@@ -19,11 +21,12 @@ class Plotter:
 
         # Plot init
 
-        self.fig = plt.figure()
+        self.fig = plt.figure(figsize=(10,10))
         self.ax1 = self.fig.add_subplot(1,1,1)
         self.ax1.set_aspect(1)
         self.ax1.set_xlim([0, self.space_width])
         self.ax1.set_ylim([0, self.space_width])
+        
 
     def plot_gen(self, gen):
         # Graficamos el estado inicial como test
@@ -37,19 +40,21 @@ class Plotter:
         first_state_u: List[float] = np.cos(first_state_velocity_dir) * first_state_velocity_mod
         first_state_v: List[float] = np.sin(first_state_velocity_dir) * first_state_velocity_mod
 
-        colors = cm.twilight(np.array(first_state_velocity_dir) / (np.pi*2))
+        colors = cm.hsv((np.array(first_state_velocity_dir) + np.pi) / (np.pi*2))
 
         # plt.scatter(first_state_x, first_state_y, s=self.space_width / 100, color='blue', alpha=0.5)
 
         self.ax1.clear()
         self.ax1.quiver(first_state_x, first_state_y, first_state_u, first_state_v, color=colors)
+        self.ax1.scatter(first_state_x, first_state_y, color=colors)
         self.ax1.set_aspect(1)
         self.ax1.set_xlim([0, self.space_width])
         self.ax1.set_ylim([0, self.space_width])
+        self.ax1.set_yticks([])
+        self.ax1.set_xticks([])
 
     def plot(self):
         
-        ani = animation.FuncAnimation(self.fig, self.plot_gen, interval=100, frames=100, repeat=False)
+        ani = animation.FuncAnimation(self.fig, self.plot_gen, interval=100, frames=len(self.states), repeat=True)
         plt.show()
         return ani
-        
