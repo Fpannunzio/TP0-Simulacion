3166cc3b391ff40329eda8cafb727cff26d151ca
Ignacio Sagues <isagues@itba.edu.ar>
2021-09-13 13:23:51 -0300

    Ej 1 con maxIterations (round=1)

--- tp3/tp3_python/ej1.py
+++ tp3/tp3_python/ej1.py
@@ -24,38 +24,36 @@ def main(data_path):
     with open(data_path, 'r') as particles_fd:
         states: List[RoundSummary] = json.load(particles_fd, object_hook=parse_state)
 
-    values = np.array(list(map(lambda state: np.array(state.collisionTimes), states)))
+    rounds = list(map(lambda state: np.array(state.collisionTimes), states))
     particleCounts = list(map(lambda state: state.particleCount, states))
 
-    rounds = values.shape[1]
-    iterations = values.shape[2]
+    roundCount = rounds[0].shape[0]
+    
+    iterations = np.array(list(map(lambda r: r.shape[1], rounds)))
+    totalTime = np.array(list(map(lambda r: r.sum(axis=1), rounds))).flatten()
 
-    frequency = iterations / values.sum(axis=2)
-
-    mean = np.mean(frequency, axis = 1)
-    std = np.std(frequency, axis = 1)
+    frequency = iterations / totalTime
 
     binSize = 0.0005
 
-    print(f'Rounds: {rounds}. Iterations: {iterations}')
-    for i in range(len(mean)):
-        print(f'Mean for N={particleCounts[i]} is: {mean[i]} and std is: {std[i]}')
-    # plotter: Plotter = Plotter(config.spaceWidth, config.iterations, states).plot()
+    print(f'Rounds: {roundCount}.')
+    for i in range(len(frequency)):
+        print(f'Freq for N={particleCounts[i]} is: {frequency[i]}. Iterations: {iterations[i]}.')
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(1, 1, 1)
     ax.set_yscale('log')
-    ax.set_title(f'BinSize: {binSize}. Rounds: {rounds}. Iterations: {iterations}.')
+    ax.set_title(f'BinSize: {binSize}. Rounds: {roundCount}.')
     ax.set_xlabel(r'$t_c$: Tiempo entre colision (s)', size=20)
     ax.set_ylabel(r'Probabilidad del intervalo', size=20)
 
     for i in range(len(particleCounts)):
-        hist, bins = np.histogram(values[i].flatten(), bins=np.arange(0, np.max(values[i]), binSize))
+        hist, bins = np.histogram(rounds[i].flatten(), bins=np.arange(0, np.max(rounds[i]), binSize))
         ax.scatter(
             bins[:-1], 
-            hist / values[i].size, 
+            hist / rounds[i].size, 
             color=cm.get_cmap('tab10')(i),
-            label=f'N= {int(particleCounts[i])}'
+            label=f'N={int(particleCounts[i])}. Iter={iterations[i]}'
         )
     plt.legend()
     plt.show()
