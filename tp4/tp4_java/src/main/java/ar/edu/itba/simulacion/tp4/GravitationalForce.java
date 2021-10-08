ba329c447eb06bc4dea61c698d9a40306063bd3b
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-08 20:21:22 -0300

    GravitationalForce: cacheamos valores para no recalcularlos demas

--- tp4/tp4_java/src/main/java/ar/edu/itba/simulacion/tp4/GravitationalForce.java
+++ tp4/tp4_java/src/main/java/ar/edu/itba/simulacion/tp4/GravitationalForce.java
@@ -2,43 +2,52 @@ package ar.edu.itba.simulacion.tp4;
 
 import static ar.edu.itba.simulacion.tp4.MolecularDynamicSolver.AXIS_DIM;
 
+import java.util.Arrays;
 import java.util.List;
 
 import ar.edu.itba.simulacion.tp4.MolecularDynamicSolver.Force;
 import ar.edu.itba.simulacion.tp4.MolecularDynamicSolver.MoleculeStateAxis;
 
 public class GravitationalForce implements Force {
-    
+
+    // Configuration
     private final List<CelestialBody>   celestialBodies;
     private final double                affectedBodyMass;
     private final double                gravitationalConstant;
+
+    // Force Cache
+    // Lo usamos porque es muy comun que todos los inputs sean iguales menos el axis. Aprovechamos que ya lo calculamos.
+    private int                   lastStateHashcode;
+    private final double[]        cachedForce;
+
 
     public GravitationalForce(final List<CelestialBody> celestialBodies, final double affectedBodyMass, final double gravitationalConstant) {
         this.celestialBodies        = celestialBodies;
         this.affectedBodyMass       = affectedBodyMass;
         this.gravitationalConstant  = gravitationalConstant;
+
+        this.cachedForce            = new double[AXIS_DIM];
     }
 
     @Override
-    public double apply(int axis, MoleculeStateAxis[] state) {
-        final int bodyCount = celestialBodies.size();
-        final double[][] forces = new double[bodyCount][AXIS_DIM];
+    public double apply(final int axis, final MoleculeStateAxis[] state) {
+        final int stateHashcode = Arrays.hashCode(state);
+        if(stateHashcode == lastStateHashcode) {
+            return cachedForce[axis];
+        }
 
-        for(int i = 0; i < bodyCount; i++) {
-            final CelestialBody body = celestialBodies.get(i);
+        lastStateHashcode = stateHashcode;
+        Arrays.fill(cachedForce, 0);
 
+        for(final CelestialBody body : celestialBodies) {
             final double distance = body.distanceTo(state[0].position, state[1].position);
             final double normalForce = affectedBodyMass * body.getMass() * gravitationalConstant / (distance * distance);
-            forces[i][0] = normalForce * ((body.getX() - state[0].position) / distance);
-            forces[i][1] = normalForce * ((body.getY() - state[1].position) / distance);
-        }
-        
-        double force = 0;
-        for(final double[] doubles : forces) {
-            force += doubles[axis];
+
+            cachedForce[0] += normalForce * ((body.getX() - state[0].position) / distance);
+            cachedForce[1] += normalForce * ((body.getY() - state[1].position) / distance);
         }
 
-        return force;
+        return cachedForce[axis];
     }
   
 }
