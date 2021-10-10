d53f124f1430844ae19273730dc7901fbc90dd52
fpannunzio <fpannunzio@itba.edu.ar>
2021-10-10 15:36:04 -0300

    Analyze specific interval for the best interval obtained in Analyze interval

--- tp4/tp4_java/src/main/java/ar/edu/itba/simulacion/tp4/marsMission/AnalyzeSpecificInterval.java
+++ tp4/tp4_java/src/main/java/ar/edu/itba/simulacion/tp4/marsMission/AnalyzeSpecificInterval.java
@@ -1,8 +1,5 @@
 package ar.edu.itba.simulacion.tp4.marsMission;
 
-import static ar.edu.itba.simulacion.tp4.marsMission.MarsMissionSimulation.*;
-import static ar.edu.itba.simulacion.tp4.marsMission.SimulationSettings.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -10,31 +7,35 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.Value;
+import ar.edu.itba.simulacion.tp4.marsMission.AnalyzeInterval.IntervalAnalysis;
+import ar.edu.itba.simulacion.tp4.marsMission.AnalyzeInterval.IterationMarsDistance;
+import ar.edu.itba.simulacion.tp4.marsMission.MarsMissionSimulation.SpaceshipInitParams;
+import ar.edu.itba.simulacion.tp4.marsMission.SimulationSettings.MarsMissionConfig;
 
-public final class AnalyzeInterval {
-    private AnalyzeInterval() {
+public class AnalyzeSpecificInterval {
+    private AnalyzeSpecificInterval() {
         // static
     }
 
     public static final double  MARS_ORBIT              = 2.2799e8;
+
+    public static final double  INITIAL_ITERATIONS      = 360 * 144; //144 intervalos de 10 min tiene un dia
     
     public static final int  MARS_ORBIT_SECONDS         = 687 * 24 * 60 * 60;
 
     public static final double  MAX_MARS_ORBITS         = 0.5;
 
-    public static final int ANALYZER_RESOLUTION_SECONDS = 24 * 60 * 60;     // 1 dia
+    public static final int ANALYZER_RESOLUTION_SECONDS = 10 * 60;     // 10 minutos
 
-    public static final int     INTERVAL_SECONDS        = 5500 * 24 * 60 * 60;  // 5500 dias
+    public static final int INTERVAL_SECONDS            = 3 * 24 * 60 * 60;  // 3 dias
 
-    public static final double DISTANCE_TOLERANCE = MARS_ORBIT / 1_00;
+    public static final double DISTANCE_TOLERANCE       = MARS_ORBIT / 1_00;
 
-    public static final int MAX_UNCHANGED_MIN_DIST = 100_000;
+    public static final int MAX_UNCHANGED_MIN_DIST      = 100_000;
 
-    public static final int INITIAL_TOLERANCE = 100_000;
+    public static final int INITIAL_TOLERANCE           = 100_000;
 
-    // public static final int MINIMUM_TOLERATED_ADVANCE = 100;
-
+    
     public static void main(String[] args) throws IOException {
         if(args.length < 1) {
             throw new IllegalArgumentException("First argument must be config path");
@@ -47,6 +48,12 @@ public final class AnalyzeInterval {
         final double dt = config.dt;
         final SpaceshipInitParams spaceshipParams = config.spaceship;
 
+        final MarsMissionSimulation baseSimulation = config.toPlanetSimulation();
+
+        baseSimulation.simulate((i, spaceship, earth, mars, sun) -> {    
+            return  i < INITIAL_ITERATIONS;
+        });
+
         final int maxIntervalIteration  = (int) (INTERVAL_SECONDS / dt) + 1;
         final int maxIterations         = (int) (MAX_MARS_ORBITS * MARS_ORBIT_SECONDS / dt) + 1;
         final int resolution            = (int) (ANALYZER_RESOLUTION_SECONDS / dt);
@@ -54,8 +61,6 @@ public final class AnalyzeInterval {
 
         final List<IterationMarsDistance> bestDistances = new LinkedList<>();
         IterationMarsDistance bestMarsDistance = null;
-
-        final MarsMissionSimulation baseSimulation = config.toPlanetSimulation();
 
         int currentIter = 0;
         while(currentIter <= maxIntervalIteration) {
@@ -104,23 +109,6 @@ public final class AnalyzeInterval {
             currentIter += resolution;
         }
 
-        mapper.writeValue(new File("output/analyze_interval.json"), new IntervalAnalysis(bestDistances, bestMarsDistance));
-    }
-
-    @Value
-    public static class IterationMarsDistance implements Comparable<IterationMarsDistance> {
-        int     startIteration;
-        double  distance;
-
-        @Override
-        public int compareTo(final IterationMarsDistance o) {
-            return o == null ? -1 : Double.compare(distance, o.distance);
-        }
-    }
-
-    @Value
-    public static class IntervalAnalysis {
-        List<IterationMarsDistance> distances;
-        IterationMarsDistance       bestDistance;
+        mapper.writeValue(new File("output/analyze_specific_interval.json"), new IntervalAnalysis(bestDistances, bestMarsDistance));
     }
 }
