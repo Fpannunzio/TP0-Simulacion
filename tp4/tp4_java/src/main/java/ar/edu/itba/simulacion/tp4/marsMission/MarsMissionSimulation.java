f813259d2d672a56cda898aaa6ad78008ca64400
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-11 07:26:39 -0300

    MarsMissionSimulation: ahora soporta return trip desde mars a earth

--- tp4/tp4_java/src/main/java/ar/edu/itba/simulacion/tp4/marsMission/MarsMissionSimulation.java
+++ tp4/tp4_java/src/main/java/ar/edu/itba/simulacion/tp4/marsMission/MarsMissionSimulation.java
@@ -8,7 +8,6 @@ import lombok.Value;
 import lombok.With;
 import lombok.extern.jackson.Jacksonized;
 
-
 public class MarsMissionSimulation {
 
     public static final int SYSTEM_DIMENSION = 2;
@@ -65,21 +64,23 @@ public class MarsMissionSimulation {
             return null;
         }
 
-        final double earthX     = earth.getX();
-        final double earthY     = earth.getY();
-        final double earthVx    = earth.getVelocityX();
-        final double earthVy    = earth.getVelocityY();
+        final CelestialBody startingPlanet = spaceshipParams.returnTrip ? mars : earth;
+        final double planetX     = startingPlanet.getX();
+        final double planetY     = startingPlanet.getY();
+        final double planetVx    = startingPlanet.getVelocityX();
+        final double planetVy    = startingPlanet.getVelocityY();
+        final double planetDistance = startingPlanet.distanceFrom0();
 
-        final double earthDistance = earth.distanceFrom0();
-        final double positionFactor = 1 + (earth.getRadius() + spaceshipParams.spaceStationDistance) / earthDistance; // (1 + d/E)
+        final int spaceshipSide = spaceshipParams.returnTrip ? -1 : 1;
+        final double positionFactor = 1 + spaceshipSide * (startingPlanet.getRadius() + spaceshipParams.spaceStationDistance) / planetDistance; // (1 + d/E)
         final double orbitalVelocity = spaceshipParams.spaceshipInitialVelocity + spaceshipParams.spaceStationOrbitalVelocity;
 
         return CelestialBody.builder()
             .withName       ("spaceship")
-            .withX          (earthX * positionFactor)
-            .withY          (earthY * positionFactor)
-            .withVelocityX  (Math.signum(earthVx) * orbitalVelocity * (Math.abs(earthY) / earthDistance) + earthVx)
-            .withVelocityY  (Math.signum(earthVy) * orbitalVelocity * (Math.abs(earthX) / earthDistance) + earthVy)
+            .withX          (planetX * positionFactor)
+            .withY          (planetY * positionFactor)
+            .withVelocityX  (spaceshipSide * Math.signum(planetVx) * orbitalVelocity * (Math.abs(planetY) / planetDistance) + planetVx)
+            .withVelocityY  (spaceshipSide * Math.signum(planetVy) * orbitalVelocity * (Math.abs(planetX) / planetDistance) + planetVy)
             .withMass       (spaceshipParams.scaledMass())
             .withRadius     (0)
             .build()
@@ -100,10 +101,11 @@ public class MarsMissionSimulation {
     }
 
     public String simulate(final SimulationStateNotifier notifier) {
-        String ret = null;
+        String collision;
 
         int iteration = 0;
         do {
+            // Actualizamos estado de cuerpos celestes
             if(spaceship != null) {
                 spaceship.update();
             }
@@ -111,10 +113,64 @@ public class MarsMissionSimulation {
             mars        .update();
             // Al sol no lo updeteamos, consideramos que esta estatico en (0, 0)
 
-            iteration++;
-        } while(notifier.notify(iteration, spaceship, earth, mars, sun) && (ret = hasSpaceshipCollided()) == null);
+            // Calculamos colisiones
+            collision = hasSpaceshipCollided();
 
-        return ret;
+            iteration++;
+        } while(notifier.notify(iteration, spaceship, earth, mars, sun) && collision == null);
+
+        return collision;
+    }
+
+    private String hasSpaceshipCollided() {
+
+        if(spaceship == null) {
+            return null;
+        }
+        else if(spaceship.hasCollided(earth)) {
+            return earth.getName();
+        }
+        else if(spaceship.hasCollided(mars)) {
+            return mars.getName();
+        }
+        else if(spaceship.hasCollided(sun)) {
+            return sun.getName();
+        }
+        else {
+            return null;
+        }
+    }
+
+    public double getSystemEnergy() {
+        // Hay que pasar la energia de kg*km^2/s^2 kg*m^2/s^2
+        return 1_000_000
+            * getCelestialBodyEnergy(earth)
+            * getCelestialBodyEnergy(mars)
+            * getCelestialBodyEnergy(spaceship)
+            ;
+    }
+
+    // TODO(tobi): Esto se esta calculando bien???
+    private double getCelestialBodyEnergy(final CelestialBody body){
+        final double potentialEnergy = ((GravitationalForce) body.getSolver().getForce()).getPotentialEnergy(body.getX(), body.getY());
+        return potentialEnergy+ body.getKineticEnergy();
+    }
+
+    public MarsMissionSimulation buildNewMission(final SpaceshipInitParams spaceshipParams) {
+        return MarsMissionSimulation.builder()
+            .withDt                     (dt)
+            .withGravitationalConstant  (gravitationalConstant)
+            .withSun                    (new CelestialBody(sun))
+            .withMars                   (new CelestialBody(mars).withSolver(
+                buildCelestialBodySolver(mars, bodiesAffectingMars()).copyState(mars.getSolver())
+            ))
+            .withEarth                  (new CelestialBody(earth).withSolver(
+                buildCelestialBodySolver(earth, bodiesAffectingEarth()).copyState(earth.getSolver())
+            ))
+            .withSpaceship              (spaceshipParams)
+            .withSolverSupplier         (solverSupplier)
+            .build()
+            ;
     }
 
     public double getDt() {
@@ -139,51 +195,6 @@ public class MarsMissionSimulation {
 
     public CelestialBody getSpaceship() {
         return spaceship;
-    }
-
-    public String hasSpaceshipCollided() {
-        if(spaceship == null) {
-            return null;
-        }
-        if(spaceship.hasCollided(earth)) {
-            return earth.getName();
-        }
-        if(spaceship.hasCollided(sun)) {
-            return sun.getName();
-        }
-        if(spaceship.hasCollided(mars)) {
-            return mars.getName();
-        }
-        return null;
-    }
-    // Hay que pasar la energia de kg*km^2/s^2 kg*m^2/s^2 
-    public double getSystemEnergy() {
-        double totalEnergy = 0;
-        totalEnergy += getCelestialBodyEnergy(earth);
-        totalEnergy += getCelestialBodyEnergy(mars);
-        totalEnergy += getCelestialBodyEnergy(spaceship);
-        return totalEnergy * 1_000_000;
-    }
-
-    private double getCelestialBodyEnergy(CelestialBody body){
-        return ((GravitationalForce)body.getSolver().getForce()).getPotentialEnergy(body.getX(), body.getY()) + 0.5 * body.getMass() * body.getVelocityModule() * body.getVelocityModule();    
-    }
-
-    public MarsMissionSimulation buildNewMission(final SpaceshipInitParams spaceshipParams) {
-        return MarsMissionSimulation.builder()
-            .withDt                     (dt)
-            .withGravitationalConstant  (gravitationalConstant)
-            .withSun                    (new CelestialBody(sun))
-            .withMars                   (new CelestialBody(mars).withSolver(
-                buildCelestialBodySolver(mars, bodiesAffectingMars()).copyState(mars.getSolver())
-            ))
-            .withEarth                  (new CelestialBody(earth).withSolver(
-                buildCelestialBodySolver(earth, bodiesAffectingEarth()).copyState(earth.getSolver())
-            ))
-            .withSpaceship              (spaceshipParams)
-            .withSolverSupplier         (solverSupplier)
-            .build()
-            ;
     }
 
     /* ----------------------------------------- Clases Auxiliares ----------------------------------------------- */
@@ -216,6 +227,7 @@ public class MarsMissionSimulation {
         public double   spaceshipInitialVelocity;
         public double   spaceStationDistance;
         public double   spaceStationOrbitalVelocity;
+        @Builder.Default public boolean  returnTrip = false;
 
         public double scaledMass() {
             return spaceshipMass * Math.pow(10, spaceshipMassScale);
