a71ade86d0838df4201e32f7b0e7778303f6ae54
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-11 18:28:06 -0300

    analyze velocities plot

---
+++ tp4/tp4_python/analyze_velocities.py
@@ -0,0 +1,46 @@
+import json
+import sys
+from typing import List
+from matplotlib import pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+
+import numpy as np
+from formater import MathTextSciFormatter
+
+from models import VelocityAnalysisInfo
+
+
+def main(data_path):
+    with open(data_path, 'r') as result_fd:
+        result: VelocityAnalysisInfo = json.load(result_fd, object_hook=VelocityAnalysisInfo.from_dict)
+
+    durations = np.array(result.tripDuration)
+    durationCount = len(durations)
+    filter = durations > 0
+    
+    velocities = np.linspace(result.initialVelocity, result.initialVelocity + result.velocityStep * durationCount, durationCount)[filter]
+    durations = durations[filter]
+
+    fig = plt.figure(figsize=(16, 10))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.yaxis.set_major_formatter(MathTextSciFormatter("%1.4e"))
+    ax.grid(which="both")
+    ax.set_axisbelow(True)
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+
+    ax.set_xlabel(r'$v_0$: Velocidad inicial de despegue (km/s)', size=20, labelpad=20)
+    ax.set_ylabel(r'Tiempo de viaje hasta colisionar (min)', size=20, labelpad=20)
+    ax.tick_params(labelsize=16)
+
+    ax.scatter(velocities, durations / 60, marker='*', s=70)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise ValueError('Config path must be given by argument')
+    try:
+        main(sys.argv[1])
+    except KeyboardInterrupt:
+        pass
--- tp4/tp4_python/models.py
+++ tp4/tp4_python/models.py
@@ -14,46 +14,14 @@ def from_dict(cls):
     setattr(cls, 'from_dict', class_from_dict)
     return cls
 
-@dataclass
-@from_dict
-class Particle:
-    id:             int
-    x:              float
-    y:              float
-    velocityMod:    float
-    velocityDir:    float
-    velocityX:      float
-    velocityY:      float
-    mass:           float
-    radius:         float
-
-class Wall(str, Enum):
-    UP = 'UP'
-    DOWN = 'DOWN'
-    LEFT = 'LEFT'
-    RIGHT = 'RIGHT'
 
 @dataclass
 @from_dict
-class Collision:
-    dTime:          float
-    particle1:      int
-    particle2:      Optional[int]
-    wall:           Optional[Wall]
+class VelocityAnalysisInfo:
+    initialVelocity:    float
+    velocityStep:       float
+    tripDuration:       List[int]
+    returnTrip:         bool
 
-@dataclass
-@from_dict
-class SimulationState:
-    time:           float
-    particles:      List[Particle]
-    collision:      Collision
-
-@dataclass
-@from_dict
-class Config:
-    spaceWidth:     float
-    iterations:     int
-    particlesFile:  str
-    outputFile:     str
 
 
