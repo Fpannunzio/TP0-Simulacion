fa2b6cd97cb8404cab96ad42d72169b4558df2eb
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-28 20:41:48 -0300

    Fix: se tenian en cuenta las dummy particles

--- tp5/tp5_java/src/main/java/ar/edu/itba/simulacion/tp5/PedestrianDynamicsSimulation.java
+++ tp5/tp5_java/src/main/java/ar/edu/itba/simulacion/tp5/PedestrianDynamicsSimulation.java
@@ -108,7 +108,7 @@ public class PedestrianDynamicsSimulation {
             locked.add(dummyParticle(-2, exitLeft));
             locked.add(dummyParticle(-1, exitRight));
 
-            calculateNextState(lockedParticles, locked::add, escaped::add);
+            calculateNextState(lockedParticles.subList(2, lockedParticles.size()), locked::add, escaped::add);
 
             lockedParticles  = locked;
             escapedParticles = escaped;
