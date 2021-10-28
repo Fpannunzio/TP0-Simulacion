9373792f218b7c7eb7ad7e342abe124e9a517e90
Tobias Brandy <tbrandy@itba.edu.ar>
2021-10-28 20:39:35 -0300

    Dummy particles para render con ovito - TODO: resolver mejor

--- tp5/tp5_java/src/main/java/ar/edu/itba/simulacion/tp5/PedestrianDynamicsSimulation.java
+++ tp5/tp5_java/src/main/java/ar/edu/itba/simulacion/tp5/PedestrianDynamicsSimulation.java
@@ -84,6 +84,19 @@ public class PedestrianDynamicsSimulation {
         this.radiusRange        = maxRadius - minRadius;
     }
 
+    private static Particle2D dummyParticle(final int id, final double x) {
+        return Particle2D.builder()
+            .withId         (-2)
+            .withX          (x)
+            .withY          (0)
+            .withVelocityMod(0)
+            .withVelocityDir(0)
+            .withMass       (1)
+            .withRadius     (0)
+            .build()
+            ;
+    }
+
     public void simulate(final List<Particle2D> initialState, final SimulationStateNotifier notifier) {
         int iteration = 0;
         List<Particle2D> lockedParticles    = initialState;
@@ -92,6 +105,8 @@ public class PedestrianDynamicsSimulation {
         while(notifier.notify(iteration, lockedParticles, escapedParticles)) {
             final List<Particle2D> locked    = new ArrayList<>(initialState.size());
             final List<Particle2D> escaped   = new LinkedList<>();
+            locked.add(dummyParticle(-2, exitLeft));
+            locked.add(dummyParticle(-1, exitRight));
 
             calculateNextState(lockedParticles, locked::add, escaped::add);
 
